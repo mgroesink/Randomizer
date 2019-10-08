7fdb6f03d854e5403695865bf01604a0102213b9
mgroesink <mroesink@rocvantwente.nl>
2019-10-08 11:16:10 +0200

    Add project files.

---
+++ Randomizer/Randomizer.cs
@@ -0,0 +1,86 @@
+﻿
+
+/// <summary>
+/// Randomizer functions
+/// </summary>
+namespace RandomizerFunctions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>Methods to generate all kinds of random things</summary>
+    public static class Randomizer
+    {
+        /// <summary>Object to generate random things</summary>
+        static readonly Random r = new Random();
+
+        #region Random Dates
+
+        /// <summary>
+        /// Get a random date between 1 januari 1900 and current date
+        /// </summary>
+        /// <returns>The generated date</returns>
+        public static DateTime RandomDate()
+        {
+            var startDate = new DateTime(1900, 1, 1);
+            var daysBetween = (DateTime.Now - startDate).Days;
+            var daysToAdd = r.Next(daysBetween);
+            return startDate.AddDays(daysToAdd);
+        }
+
+        /// <summary>
+        /// Get a random date between a given date and the current date
+        /// </summary>
+        /// <returns>The generated date</returns>
+        public static DateTime RandomDate(DateTime startDate)
+        {
+            if (startDate >= DateTime.Now)
+            {
+                throw new ArgumentException("Startdate cannot be a date in the future");
+            }
+            var daysBetween = (DateTime.Now - startDate).Days;
+            var daysToAdd = r.Next(daysBetween);
+            return startDate.AddDays(daysToAdd);
+        }
+
+        /// <summary>
+        /// Get a random date between 2 given dates
+        /// </summary>
+        /// <returns>The generated date</returns>
+        public static DateTime RandomDate(DateTime startDate, DateTime endDate)
+        {
+            if (startDate >= endDate)
+            {
+                throw new ArgumentException("Startdate must be before enddate");
+            }
+            var daysBetween = (endDate - startDate).Days;
+            var daysToAdd = r.Next(daysBetween);
+            return startDate.AddDays(daysToAdd);
+        }
+        #endregion
+
+        #region Random Integer
+        /// <summary>Gets a random integer.</summary>
+        /// <param name="useNegative">if set to <c>true</c> [use negative].</param>
+        /// <returns>The generated number</returns>
+        public static int RandomInteger(bool useNegative)
+        {
+            int minValue;
+            int maxValue = int.MaxValue;
+            if (useNegative)
+            {
+                minValue = int.MinValue;
+            }
+            else
+            {
+                minValue = 0;
+            }
+            return r.Next(minValue + 1, maxValue);
+        } 
+        #endregion
+
+    }
+}
---
+++ RandomizerTests/RandomizerTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomizerFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizerFunctions.Tests
+{
+    [TestClass()]
+    public class RandomizerTests
+    {
+        [TestMethod()]
+        public void RandomDateTest()
+        {
+            var randomDate = RandomizerFunctions.Randomizer.RandomDate();
+            var startDate = new DateTime(1900, 1, 1);
+            var valid = true;
+            for (var i = 0; i < 100000; i++)
+            {
+                randomDate = RandomizerFunctions.Randomizer.RandomDate();
+                if (randomDate < startDate || randomDate >= DateTime.Now) valid = false;
+            }
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod()]
+        public void RandomDateTest1()
+        {
+            var startDate = new DateTime(1958, 1, 10);
+            var randomDate = RandomizerFunctions.Randomizer.RandomDate(startDate);
+
+            var valid = true;
+            for (var i = 0; i < 100000; i++)
+            {
+                randomDate = RandomizerFunctions.Randomizer.RandomDate(startDate);
+                if (randomDate < startDate || randomDate >= DateTime.Now) valid = false;
+            }
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod()]
+        public void RandomDateTest2()
+        {
+            var startDate = new DateTime(1958, 1, 10);
+            var endDate = new DateTime(1972, 12, 31);
+            var randomDate = RandomizerFunctions.Randomizer.RandomDate(startDate, endDate);
+
+            var valid = true;
+            for (var i = 0; i < 100000; i++)
+            {
+                randomDate = RandomizerFunctions.Randomizer.RandomDate(startDate, endDate);
+                if (randomDate < startDate || randomDate >= DateTime.Now) valid = false;
+            }
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EndBeforeStartTest()
+        {
+            var endDate = new DateTime(1958, 1, 10);
+            var startDate = new DateTime(1972, 12, 31);
+            var randomDate = RandomizerFunctions.Randomizer.RandomDate(startDate, endDate);
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EndBeforeStartTest2()
+        {
+            var startDate = DateTime.Now.AddDays(1);
+            var randomDate = RandomizerFunctions.Randomizer.RandomDate(startDate);
+
+        }
+
+        [TestMethod()]
+        public void RandomIntegerTest()
+        {
+            var valid = true;
+            int randomInteger;
+            for (var i = 0; i < 100000; i++)
+            {
+                randomInteger = Randomizer.RandomInteger(true);
+                if (randomInteger <= int.MinValue ||
+                    randomInteger >= int.MaxValue)
+                {
+                    valid = false;
+                }
+            }
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod()]
+        public void RandomPositiveIntegerTest()
+        {
+            var valid = true;
+            int randomInteger;
+            for (var i = 0; i < 100000; i++)
+            {
+                randomInteger = Randomizer.RandomInteger(false);
+                if (randomInteger <= 0 ||
+                    randomInteger >= int.MaxValue)
+                {
+                    valid = false;
+                }
+            }
+            Assert.IsTrue(valid);
+        }
+    }
+}
